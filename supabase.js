12d1eeef00215522116cb69e107ed727b3f51795
hanarion1 <hanarion1@gmail.com>
2026-07-18 08:44:37 +0800

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://wamjmozodpjvegbfqrth.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_No4fqVlfurqdAZwz-18Xhw_4HIs1Ecl";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
